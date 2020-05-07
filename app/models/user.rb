d6d7bba913875e8d25e2ccff035120d3c00ec415
ahmedhassanien503 <ahm_hassanien@yahoo.com>
2020-05-07 16:39:36 +0200

    Add Active admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  rolify
+  # rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
