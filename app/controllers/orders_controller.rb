899e860d461c604570792fec11e9764132065214
MayarAlaa1 <mayar.alaa188@gmail.com>
2020-05-09 15:30:21 +0200

    before merge

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -30,9 +30,20 @@ class OrdersController < ApplicationController
   # POST /orders.json
   def create
     @order = Order.new(order_params)
+    @current_cart.line_items.each do |item|
+      @order.line_items << item
+      item.cart_id = nil
+    end
+
     @order.orderstate = "pending"
     # @order.user_id = 1 #current_user_id
     @order.user_id = current_user.id
+    @order.save
+    Cart.destroy(session[:cart_id])
+    session[:cart_id] = nil
+
+
+   
   
 
     respond_to do |format|
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_05_08_153604) do
-# ActiveRecord::Schema.define(version: 2020_05_07_213943) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
