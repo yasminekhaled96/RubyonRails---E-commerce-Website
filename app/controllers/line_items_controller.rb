307e3746fb57a19a28dae6f4ee38091f092f21c8
MarwaHafiz <marwahafiz1996@gmail.com>
2020-05-10 00:32:02 +0200

    cart show

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -27,20 +27,20 @@ class LineItemsController < ApplicationController
         redirect_to cart_path(@current_cart)
       end 
       def add_quantity
-        @line_item = LineItem.find(params[:id])
-        @line_item.quantity += 1
-        @line_item.save
-        redirect_to cart_path(@current_cart)
-      end
-      
-      def reduce_quantity
-        @line_item = LineItem.find(params[:id])
-        if @line_item.quantity > 1
-          @line_item.quantity -= 1
-        end
-        @line_item.save
-        redirect_to cart_path(@current_cart)
-      end
+  @line_item = LineItem.find(params[:id])
+  @line_item.quantity += 1
+  @line_item.save
+  redirect_to cart_path(@current_cart)
+end
+
+def reduce_quantity
+  @line_item = LineItem.find(params[:id])
+  if @line_item.quantity > 1
+    @line_item.quantity -= 1
+  end
+  @line_item.save
+  redirect_to cart_path(@current_cart)
+end
       private
         def line_item_params
           params.require(:line_item).permit(:quantity,:product_id, :cart_id)
