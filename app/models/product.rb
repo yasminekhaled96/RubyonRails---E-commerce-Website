397a05eef6440b0d7a2d8bb55d836d6e1b1a051a
yasminekhaled96 <yasmina.khaled96@gmail.com>
2020-05-09 13:55:24 +0200

    home page demo,upload image using active storage

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,8 @@ class Product < ApplicationRecord
   belongs_to :category
   belongs_to :brand
   has_and_belongs_to_many :orders
-  # has_one_attached: image
+  has_one_attached :image
+
   belongs_to :store
   belongs_to :user
 
